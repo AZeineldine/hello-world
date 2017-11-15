0948b440417cba3f95914f3370076a12f1014b21
AlaaZeineldine <azeineldine@hotmail.com>
2017-11-15 02:57:59 +0200

    Made a longer greeting

--- HelloWorld.cs
+++ HelloWorld.cs
@@ -6,7 +6,7 @@ namespace hello_world
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello World!" + "\nl" +"Let's see how smart you are, World!");
         }
     }
 }
